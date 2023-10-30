e80a4d98f208f5341e199fca097ca8cf810a2d4d
SejoMont <sejo.monterocastro@gmail.com>
2023-10-30 12:10:40 +0100

    vFinal

--- src/components/ModificarPersonaje.js
+++ src/components/ModificarPersonaje.js
@@ -8,7 +8,11 @@ import { Navigate } from 'react-router-dom';
 export default class ModificarPersonaje extends Component {
   state = {
     status: false,
-    serie: ""
+    statusSerie: false,
+    statusPersonaje: false,
+    serie: "",
+    serieSelecionada: [],
+    personajeSeleccionado: [],
   }
 
   cajaSerie = React.createRef();
@@ -31,30 +35,73 @@ export default class ModificarPersonaje extends Component {
     })
   }
 
+  serieSeleccionada = () => {
+    var request = "api/Series/" + parseInt(this.cajaSerie.current.value);
+    var url = Global.apiSeries + request;
+
+    axios.get(url).then(response => {
+      this.setState({
+        serieSelecionada: response.data,
+        statusSerie: true,
+      })
+    })
+  }
+
+  personajeSeleccionado = () => {
+    var request = "api/Personajes/" + parseInt(this.cajaPersonaje.current.value);
+    var url = Global.apiSeries + request;
+
+    axios.get(url).then(response => {
+      this.setState({
+        personajeSeleccionado: response.data,
+        statusPersonaje: true,
+      })
+    })
+  }
+
   render() {
     return (
-      <div>
+      <div style={{ width: "500px", margin: "auto" }}>
         {
           this.state.status == true && (
             <Navigate to={"/personajes/" + this.state.serie} />
           )
         }
+
         <h1 className='text-center'>Modificar Personaje</h1>
-        <form style={{ width: "500px", margin: "auto" }}>
+
+        <form>
           <label>Serie: </label>
-          <select ref={this.cajaSerie} className="form-select">
+          <select ref={this.cajaSerie} className="form-select" onChange={this.serieSeleccionada}>
             <CargarSeriesForm />
           </select>
           <label>Personaje: </label>
-          <select ref={this.cajaPersonaje} className="form-select">
+          <select ref={this.cajaPersonaje} className="form-select" onChange={this.personajeSeleccionado}>
             <CargarPersonajesForm />
           </select>
           <br></br>
           <div className='text-end'>
             <button onClick={this.modificarPersonaje} className='btn btn-outline-success '>Crear</button>
           </div>
-
         </form>
+
+        {
+          this.state.statusSerie == true && (
+            <div>
+              <h1>{this.state.serieSelecionada.nombre}</h1>
+              <img src={this.state.serieSelecionada.imagen} width={"500px"}></img>
+            </div>
+          )
+        }
+
+        {
+          this.state.statusPersonaje == true && (
+            <div>
+              <h1>{this.state.personajeSeleccionado.nombre}</h1>
+              <img src={this.state.personajeSeleccionado.imagen} width={"500px"}></img>
+            </div>
+          )
+        }
       </div>
     )
   }
